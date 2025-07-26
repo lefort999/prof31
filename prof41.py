fab8aa66813c781a7749b8a17b7fd1d00a1f55f8
lefort999 <jlefort251@gmail.com>
2025-07-26 16:45:23 +0200

    Update prof41.py

--- prof41.py
+++ prof41.py
@@ -95,9 +95,9 @@ def recherche():
 
             if profession == "orfèvre":
                 if lieu.lower() == "paris":
-                msg.append("💎 Orfèvre parisien : consulter les registres de poinçons de la capitale.")
-    else:
-                msg.append("💎 Orfèvre en province : consulter les registres de poinçons régionaux, ils sont différents de ceux de Paris.")
+                    msg.append("💎 Orfèvre parisien : consulter les registres de poinçons de la capitale.")
+     else:
+                    msg.append("💎 Orfèvre en province : consulter les registres de poinçons régionaux, ils sont différents de ceux de Paris.")
 
             if circonstance == "inondation":
                 msg.append("🌊 En 1910 Paris sous les eaux.")
