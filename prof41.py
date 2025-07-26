239429407bf0145b43fcb09742f5a95aa4f490e0
lefort999 <jlefort251@gmail.com>
2025-07-26 16:21:02 +0200

    Update prof41.py

--- prof41.py
+++ prof41.py
@@ -93,8 +93,11 @@ def recherche():
             if rubrique == "Alsace":
                 msg.append("🇩🇪 Né en Alsace entre 1870 et 1918 : consulter ANOM ou archives allemandes.")
 
-            if profession == "orfèvre" and lieu == "paris":
-                msg.append("💎 Orfèvre parisien : consulter les registres de poinçons de la capitale.")
+            if profession == "orfèvre":
+            if lieu.lower() == "paris":
+               msg.append("💎 Orfèvre parisien : consulter les registres de poinçons de la capitale.")
+    else:
+              msg.append("💎 Orfèvre en province : consulter les registres de poinçons régionaux, ils sont différents de ceux de Paris.")
 
             if circonstance == "inondation":
                 msg.append("🌊 En 1910 Paris sous les eaux.")
